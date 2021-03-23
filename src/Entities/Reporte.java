794e4dadf9086d7ff6cdcdf5b935b8f93fb330ac
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-03-23 00:07:47 -0600

    Implemented the enumeration TipoReporte. It lists all the different types of reports. Started implementing Class Reporte.

---
+++ src/Entities/Reporte.java
@@ -0,0 +1,19 @@
+/*
+ * Autor: Christian Felipe de Jesus Avila Valdes
+ * Versión: 1.0
+ * Fecha Creación: 3 - mar - 2021
+ * Descripción:
+ * Clase que contiene la información de Reporte
+ */
+package Entities;
+
+import Enumerations.TipoReporte;
+
+public class Reporte extends Documento {
+    private int horasReportadas;
+    private TipoReporte tipo;
+
+    public Reporte () {
+
+    }
+}
