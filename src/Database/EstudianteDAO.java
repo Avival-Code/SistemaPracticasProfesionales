566b2f4f63a7baf20e7ff5839b73807a60633f25
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-03-23 20:02:22 -0600

    Started implementing EstudianteDAO, but realized that it would be more efficient to implement UsuarioUVDAO first. EstudianteDAO has been put on hold. Created UsuarioUVDAOInterface. No methods added yet.

--- src/Database/EstudianteDAO.java
+++ src/Database/EstudianteDAO.java
@@ -10,31 +10,42 @@
 package Database;
 
 import Entities.Estudiante;
+import java.sql.Statement;
 import java.util.List;
 
 public class EstudianteDAO implements EstudianteDAOInterface{
     @Override
-    public boolean Create(Estudiante estudiante) {
-        return false;
+    public boolean Create( Estudiante estudiante ) {
+        boolean fueCreado = false;
+        MySqlConnection connection = new MySqlConnection();
+        connection.StartConnection();
+
+        try {
+            Statement statement = connection.GetConnection().createStatement();
+            String query = "INSERT INTO Estudiante()";
+        } catch( Exception exception ) {
+            exception.printStackTrace();
+        }
+        return fueCreado;
     }
 
     @Override
-    public List<Estudiante> ReadAll() {
+    public List< Estudiante > ReadAll() {
         return null;
     }
 
     @Override
-    public Estudiante ReadEstudiante(String matricula) {
+    public Estudiante ReadEstudiante( String matricula ) {
         return null;
     }
 
     @Override
-    public boolean Update(Estudiante estudianteActualizado) {
+    public boolean Update( Estudiante estudianteActualizado ) {
         return false;
     }
 
     @Override
-    public boolean Delete(String matricula) {
+    public boolean Delete( String matricula ) {
         return false;
     }
 }
