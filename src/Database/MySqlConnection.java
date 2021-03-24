0b89a4f6f8aace3fe30c5db550f3dc8188e150ff
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-03-23 18:42:06 -0600

    Created MySqlConnection file to begin implementing the database connection. Implemented InformationReader to separate the necessary sensitive information used in a database connection from the code. InformationReader reads the specified file and stores the information in an array list.

---
+++ src/Database/MySqlConnection.java
@@ -0,0 +1,12 @@
+/*
+ * Autor: Christian Felipe de Jesus Avila Valdes
+ * Versión: 1.0
+ * Fecha Creación: 4 - mar - 2021
+ * Descripción:
+ * Clase encargada de manejar la conexión con la
+ * base de datos de mysql.
+ */
+package Database;
+
+public class MySqlConnection {
+}
