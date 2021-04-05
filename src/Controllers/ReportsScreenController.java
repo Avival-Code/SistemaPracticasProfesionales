52b4674a0b5b3c9b57fcdb771f4c83cc406211db
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-04-05 18:31:44 -0500

    Implemented ConfigureFileChooser. SetCellValurFactory and ConfigureFileChooser are called in initialize method. Starting to flesh out TurnInReport method.

--- src/Controllers/ReportsScreenController.java
+++ src/Controllers/ReportsScreenController.java
@@ -1,22 +1,27 @@
 package Controllers;
 
+import Database.ReporteDAO;
 import Entities.Reporte;
 import Utilities.ScreenChanger;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import sample.LoginSession;
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ReportsScreenController implements Initializable {
-    private FileChooser fileChoose = new FileChooser();
+    private FileChooser fileChooser = new FileChooser();
     private ScreenChanger screenChanger = new ScreenChanger();
+    private ReporteDAO reportes = new ReporteDAO();
 
     @FXML
     private Text nameText;
@@ -51,6 +56,8 @@ public class ReportsScreenController implements Initializable {
     @Override
     public void initialize( URL url, ResourceBundle resourceBundle ) {
         SetUserInformation();
+        SetCellValueFactory();
+        ConfigureFileChooser();
     }
 
     private void SetUserInformation() {
@@ -60,9 +67,17 @@ public class ReportsScreenController implements Initializable {
     }
 
     private void SetCellValueFactory() {
-        nameColumn.setCellValueFactory( new PropertyValueFactory<>( "nombre" ) );
-        keyColumn.setCellValueFactory( new PropertyValueFactory<>( "nombre" ) );
-        dateColumn.setCellValueFactory( new PropertyValueFactory<>( "nombre" ) );
+        nameColumn.setCellValueFactory( new PropertyValueFactory<>( "titulo" ) );
+        keyColumn.setCellValueFactory( new PropertyValueFactory<>( "idDocumento" ) );
+        dateColumn.setCellValueFactory( new PropertyValueFactory<>( "fechaEntrega" ) );
+    }
+
+    private void ConfigureFileChooser() {
+        fileChooser.setTitle( "Buscar Reporte..." );
+    }
+
+    private void ShowReports() {
+
     }
 
     @FXML
@@ -72,6 +87,12 @@ public class ReportsScreenController implements Initializable {
 
     @FXML
     public void TurnInReport( MouseEvent mouseEvent ) {
+        File report = GetFile( mouseEvent );
+        if( report != null ) {
+        }
+    }
 
+    private File GetFile( MouseEvent mouseEvent ) {
+        return fileChooser.showOpenDialog( ( (Node)mouseEvent.getSource() ).getScene().getWindow() );
     }
 }
