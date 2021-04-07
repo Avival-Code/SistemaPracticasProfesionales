2a6c7f206f03e305b11a13730b3a78c83148b3c6
Dan <olveradan@hotmail.com>
2021-04-06 23:18:07 -0500

    Mostrar estudiantes de un grupo en una tabla
    Se realizo el codigo necesario para mostrar a los estudiantes del grupo del Docente, para esto se tuvo que modificar algunas entidades (ArchivoConsulta, Estudiante, UsuarioUV) aunque solo fueron sus getters, ya que el CellFactory de la TableColumn no detectaba los metodos iniciando con mayuscula

--- src/Controllers/Principal_Docente.java
+++ src/Controllers/Principal_Docente.java
@@ -8,20 +8,21 @@
  */
 package Controllers;
 
-import Database.DocenteDAO;
 import Database.EstudianteDAO;
-import Entities.Docente;
+import Entities.ArchivoConsulta;
 import Entities.Estudiante;
+import Entities.Proyecto;
+import Entities.UsuarioUV;
 import Utilities.ScreenChanger;
-import javafx.beans.Observable;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Arc;
 import javafx.scene.text.Text;
 import sample.LoginSession;
 
@@ -34,6 +35,7 @@ public class Principal_Docente implements Initializable {
 
     private EstudianteDAO estudianteDAO = new EstudianteDAO();
     private List< Estudiante > grupo = new ArrayList< Estudiante >();
+    private List< ArchivoConsulta > archivoConsultas = new ArrayList< ArchivoConsulta >();
     private ScreenChanger screenChanger = new ScreenChanger();
 
     @FXML
@@ -47,25 +49,61 @@ public class Principal_Docente implements Initializable {
     @FXML
     private Label lbCedulaProfesional;
     @FXML
-    private TableView tbvGrupo;
+    private TableView< UsuarioUV > tbvGrupo;
     @FXML
-    private TableColumn tcMatricula;
+    private TableColumn< Estudiante, String > tcMatricula;
     @FXML
-    private TableColumn tcNombre;
+    private TableColumn< UsuarioUV, String > tcNombre;
     @FXML
-    private TableColumn tcProyectoAsignado;
+    private TableColumn<Proyecto, String > tcProyectoAsignado;
     @FXML
-    private TableView tbvArchivosSubidos;
+    private TableView< ArchivoConsulta > tbvArchivosSubidos;
     @FXML
-    private TableColumn tcNombreArchivo;
+    private TableColumn< ArchivoConsulta, String > tcNombreArchivo;
     @FXML
-    private TableColumn tcDescripcion;
+    private TableColumn< ArchivoConsulta, String > tcDescripcion;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         //TODO
         SetUsuario();
         RecuperarGrupo();
+        ConfigurarColumnasTablas();
+        MostrarGrupo();
+        MostrarArchivosSubidos();
+    }
+
+    public void MostrarArchivosSubidos() {
+        tbvArchivosSubidos.getItems().clear();
+        for( ArchivoConsulta archivoConsulta : archivoConsultas ) {
+            tbvArchivosSubidos.getItems().add( archivoConsulta );
+        }
+    }
+
+    /**
+     * Muestra los estudiantes pertenecientes al grupo asignado al docente.
+     */
+    public void MostrarGrupo() {
+        tbvGrupo.getItems().clear();
+        for( Estudiante estudiante : grupo ) {
+            tbvGrupo.getItems().add( estudiante );
+        }
+    }
+
+    /**
+     * Se configuran las columnas de las tablas, indicando que atributos de la entidad
+     * van a ser mostrados por cada columna.
+     */
+    public void ConfigurarColumnasTablas() {
+        //TODO
+        //Tabla de grupo
+        tcMatricula.setCellValueFactory( new PropertyValueFactory<>("matricula") );
+        tcNombre.setCellValueFactory( new PropertyValueFactory<>("nombre") );
+        //tcProyectoAsignado.setCellValueFactory( new PropertyValueFactory<>("") );
+
+        //Tabla de archivos subidos por el docente
+        tcNombreArchivo.setCellValueFactory( new PropertyValueFactory<>("titulo") );
+        //tcDescripcion.setCellValueFactory( new PropertyValueFactory<>("descripcion") );
     }
 
     /**
@@ -82,7 +120,7 @@ public class Principal_Docente implements Initializable {
      * y numero personal.
      */
     public void SetUsuario() {
-        lbNombre.setText( LoginSession.GetInstance().GetDocente().GetNombres() );
+        lbNombre.setText( LoginSession.GetInstance().GetDocente().getNombres() );
         lbApellidos.setText( LoginSession.GetInstance().GetDocente().GetApellidos() );
         lbCedulaProfesional.setText( LoginSession.GetInstance().GetDocente().GetNumeroPersonal() );
     }
